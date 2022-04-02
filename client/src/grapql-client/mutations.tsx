9b8cab99112a578756b69d8c890039705f00fb9c
votrieuvy1661 <votrieuvy1661@gmail.com>
2022-04-02 20:25:23 +0700

    password function

--- client/src/grapql-client/mutations.tsx
+++ client/src/grapql-client/mutations.tsx
@@ -12,6 +12,7 @@ const login = gql`
     mutation login($username: String!, $password: String!) {
         login(username: $username, password: $password) {
             username
+            token
         }
     }
 `;
